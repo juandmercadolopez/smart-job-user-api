dd35e37cbfef0177b0925cd8aee2b5a0bc7bbddf
Juan Mercado <juandmercadolopez@gmail.com>
2024-09-10 01:42:13 -0500

    c013 : Se actualiza mensaje en message

--- src/main/java/cl/bci/user/constant/MessageConstant.java
+++ src/main/java/cl/bci/user/constant/MessageConstant.java
@@ -2,6 +2,6 @@ package cl.bci.user.constant;
 
 public class MessageConstant {
 
-    public static final String USER_NOT_FOUND = "User don't exist";
+    public static final String USER_NOT_FOUND = "The user doesn't exist";
 
 }
